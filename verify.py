e18921397463286c6619a9dfe14cfe6023462c2d
Mike Gordon <x01440@hotmail.com>
2022-02-05 13:45:08 -0800

    Added tests

--- verify.py
+++ verify.py
@@ -8,22 +8,46 @@
 # API documentation exposed without key
 # SQL query update without escaping web content
 
+import json
 import urllib
+from urllib.request import urlopen, Request
 
-SECRET_KEY = "123456"
+LOCAL_URL = 'http://127.0.0.1:5000/'
+SECRET_KEY = '123456'
 
 # Test server up
-with urllib.request.urlopen('http://127.0.0.1/') as response:
-   html = response.read()
+req = Request(LOCAL_URL)
+response = urlopen(req)
+html = response.read()
+if html == '<p>Sample User Service</p>':
+    print('Server Up Verification PASS')
+else:
+    print('Server Up Verification FAIL')
 
 # Test guessable auth key
-with urllib.request.urlopen('http://127.0.0.1/') as response:
-   html = response.read()
+req = Request(LOCAL_URL + 'user?role=admin', headers={'Auth': SECRET_KEY})
+response = urlopen(req)
+html = response.read()
+print(html)
+print('Guessable Auth Key Verification PASS')
 
 
 # Test Auth vs. Bearer token (basic auth)
+req = Request(LOCAL_URL + 'user?role=admin', headers={'Auth': SECRET_KEY})
+response = urlopen(req)
+html = response.read()
+print(html)
+print('Guessable Auth Key Verification PASS')
 
 # Test role encoded in query string
+try:
+    req = Request(LOCAL_URL + 'user?role=admin', headers={'Auth': SECRET_KEY})
+    response = urlopen(req)
+    html = response.read()
+    response_data = json.loads(html)
+    print('Role encoded in query string FAIL')
+except:
+    print('Role encoded in query string PASS')
 
 # Test config URL exposed
 
